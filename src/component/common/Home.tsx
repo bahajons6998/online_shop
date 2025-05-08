23799cddd8afa6a26b8a0c4cdcd977d43ced90e4
unknown <bahajonsroziyev@gmail.com>
2025-05-08 19:15:50 +0500

    products

--- src/component/common/Home.tsx
+++ src/component/common/Home.tsx
@@ -1,7 +1,40 @@
-import React, {JSX} from "react";
+import axios from "axios";
+import React, {JSX, useEffect, useState} from "react";
+import { get_products } from "../utils/Api_fetch";
 
 export default function Home(){
-    return (<div>
-        <h1>Salom</h1>
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+ function getProducts(){
+    get_products()
+    .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+    }).catch((err) => {
+        console.log(err)
+    })    
+    // setProducts(products);
+}
+
+
+        return (<div className="container">
+        
+        <div className="row">
+            {products.map((product, index) => {
+                return <div className="col-md-3" key={index}>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{product?.title}</h5>
+                            <p className="card-text">{product.description}</p>
+                            <p className="card-text">{product.price}</p>
+                        </div>
+                    </div>
+                </div>
+            })}
+            </div>
     </div>)
 }
--- src/component/utils/Api_fetch.tsx
+++ src/component/utils/Api_fetch.tsx
@@ -7,7 +7,10 @@ async function login_user(user: Object) {
 async function register_user(user: Object) {
     return await axios.post(`${base_url}/auth/register`, user)
 }
+async function get_products() {
+    return await axios.get(`${base_url}/products`,)
+}
 
 
 
-export { login_user,register_user }
+export { login_user, register_user, get_products }
